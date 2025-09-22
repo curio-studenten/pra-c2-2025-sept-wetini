3458c0cc800c88a4f65a39ab6eedcb4829ddfeba
HyperNeutron <nigel.koremans@gmail.com>
2025-09-22 12:26:09 +0200

    added missing translations

---
+++ resources/lang/nl/about.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'about_title' => "Over ons",
+    'contactform_link' => "Om direct met ons contact op te nemen, kan dit <a href=':url'>hier</a>",
+    'socials' => "Onze socials",
+    'about_content' => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae, accusantium unde vitae dignissimos nobis reiciendis assumenda aliquid amet beatae corrupti blanditiis ea molestiae inventore iste explicabo qui dolorem earum, nulla sequi sit voluptatem. Dignissimos, quis necessitatibus, minima aliquam qui veniam soluta reprehenderit, tempora maiores dolores veritatis optio repellat porro quaerat?",
+];
